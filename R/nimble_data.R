35c29702b21c28aca2d27540a926202c8d423c4a
John R. Foster, PhD <30320569+JohnRFoster@users.noreply.github.com>
2026-04-07 17:01:55 -0600

    data

---
+++ R/nimble_data.R
@@ -0,0 +1,23 @@
+#' Prepare data for nimble model
+#' @param df A data frame containing the survey data.
+#' @return A list of data for the nimble model.
+#' @export
+
+nimble_data <- function(df) {
+	# mean litter size year from VerCauteren et al. 2019 pg 63
+	data_ls <- read_rds("data/litterSizeVerCauterenEtAl2019.rds")
+
+	X <- create_X(df)
+	y_sum <- removed_in_pp_cumsum(df)
+
+	list(
+		y = df$take,
+		y_sum = y_sum,
+		J = data_ls,
+		X_p = X,
+		effort_per = df$effort_per,
+		log_effort_per = log(df$effort_per),
+		n_trap_m1 = df$trap_count - 1,
+		log_survey_area_km2 = log(df$property_area_km2)
+	)
+}
